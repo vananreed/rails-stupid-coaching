e35144500122bb66df12c0bc8b249631aec7f75c
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-07-26 13:04:47 +0800

    question controller and routes added. Functional stupid coach.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'questions#ask'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
